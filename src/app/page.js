439b273383aa662dbe41846d9ef9958e40e41376
Cristea Bianca <bianca.cristea01@yahoo.com>
2025-03-23 18:42:22 +0200

    Fix seo

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import ClientResearchView from "@/components/client-view/research";
 import ClientTeachingView from "@/components/client-view/teaching";
 import ClientCompetitiveProgrammingView from "@/components/client-view/competitive-programming";
 import ClientPassionsView from "@/components/client-view/passions";
+import Head from "next/head";
 
 const componentMap = {
   home: ClientHomeView,
@@ -71,18 +72,42 @@ export default function Page() {
 
   return (
     <>
-      <head>
+      <Head>
         <title>Profesor Universitatea din București - Marius Dumitran</title>
         <meta
           name="description"
-          content="Pagina oficială a profesorului Marius Dumitran de la Universitatea din București"
+          content="Pagina profesorului Marius Dumitran de la Universitatea din București"
         />
         <meta
           name="keywords"
-          content="Marius Dumitran, profesor, UniBuc, educație, UB"
+          content="Marius Dumitran, profesor, UniBuc, Universitatea din București, educație, UB"
         />
         <meta name="robots" content="index, follow" />
-      </head>
+        <meta
+          property="og:title"
+          content="Profesor Universitatea din București - Marius Dumitran"
+        />
+        <meta
+          property="og:description"
+          content="Pagina profesorului Marius Dumitran de la Universitatea din București"
+        />
+        <meta
+          property="og:url"
+          content="https://profesor-unibuc-dumitranmarius.vercel.app/"
+        />
+        <meta property="og:image" content="URL_imagine.jpg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Profesor Universitatea din București - Marius Dumitran"
+        />
+        <meta
+          name="twitter:description"
+          content="Pagina profesorului Marius Dumitran de la Universitatea din București"
+        />
+        <meta name="twitter:image" content="URL_imagine.jpg" />
+      </Head>
       <div className="flex h-screen">
         <SidebarMenu
           setActiveComponent={setActiveComponent}
